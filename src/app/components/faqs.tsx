113a08fc3e312f3b4dc0f44d373f672e3fe89ac0
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2024-12-08 03:30:25 +0100

    add faqs

--- src/app/components/faqs.tsx
+++ src/app/components/faqs.tsx
@@ -1,7 +1,37 @@
+import { MinusIcon } from "~/components/icons/minus";
+import { PlusIcon } from "~/components/icons/plus";
 import { StarsIcon } from "~/components/icons/stars";
 import { UsersIcon } from "~/components/icons/users";
 import { Badge } from "~/components/ui/badge";
 import { SectionIcon } from "~/components/ui/section-icon";
+
+const faqs = [
+  {
+    question: "What is DesignWhiz",
+    answer:
+      "To join DesignWhiz, simply visit our <a class='underline underline-offset-4' href='https://mydesignwhiz.com'>website</a> and sign up for an account. It's free and easy to get started! Join in now and get your money’s worth for 20% off.",
+  },
+  {
+    question: "How does DesignWhiz works?",
+    answer:
+      "To join DesignWhiz, simply visit our <a class='underline underline-offset-4' href='https://mydesignwhiz.com'>website</a> and sign up for an account. It's free and easy to get started! Join in now and get your money’s worth for 20% off.",
+  },
+  {
+    question: "What are the benefits of DesignWhiz Pro?",
+    answer:
+      "To join DesignWhiz, simply visit our <a class='underline underline-offset-4' href='https://mydesignwhiz.com'>website</a> and sign up for an account. It's free and easy to get started! Join in now and get your money’s worth for 20% off.",
+  },
+  {
+    question: "How do I upgrade to DsgnWhiz Pro?",
+    answer:
+      "To join DesignWhiz, simply visit our <a class='underline underline-offset-4' href='https://mydesignwhiz.com'>website</a> and sign up for an account. It's free and easy to get started! Join in now and get your money’s worth for 20% off.",
+  },
+  {
+    question: "Can I cancel my DsgnWhiz Pro subscription?",
+    answer:
+      "To join DesignWhiz, simply visit our <a class='underline underline-offset-4' href='https://mydesignwhiz.com'>website</a> and sign up for an account. It's free and easy to get started! Join in now and get your money’s worth for 20% off.",
+  },
+];
 
 export function FAQs() {
   return (
@@ -31,7 +61,44 @@ export function FAQs() {
           </div>
         </div>
       </div>
-      <div className="container mx-auto mt-14"></div>
+      <div className="container mx-auto px-4 mt-14">
+        <ul className="space-y-4 max-w-screen-md mx-auto">
+          {faqs.map((qa, idx) => {
+            return (
+              <li key={idx} className="faq-item">
+                <details
+                  className="bg-background rounded-xl overflow-hidden group"
+                  key={idx}
+                >
+                  <summary className="justify-between items-center flex gap-4 p-5 cursor-pointer">
+                    <h3 className="tracking-tight font-medium flex-1 text-[#1B0947]">
+                      {qa.question}
+                    </h3>
+                    <span className="flex-shrink-0">
+                      <span className="transition-all opacity-0 group-open:opacity-100">
+                        <MinusIcon />
+                      </span>
+                      <span className="transition-all opacity-100 group-open:opacity-0">
+                        <PlusIcon />
+                      </span>
+                    </span>
+                  </summary>
+                  <div
+                    className="faq-item__content p-5 pt-0 text-[#1B0947B2]"
+                    dangerouslySetInnerHTML={{ __html: qa.answer }}
+                  />
+                </details>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="text-center text-sm tracking-tighter mt-10">
+          Have more questions?{" "}
+          <a href="#" className="text-[#7421FC]">
+            Get Help
+          </a>
+        </div>
+      </div>
     </section>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Footer } from "~/components/footer";
 import { DsgnWhizIcon } from "~/components/icons/dsgnwhiz";
 import { Button } from "~/components/ui/button";
 import { Benefits } from "./components/benefits";
@@ -11,35 +12,40 @@ import { Testimonials } from "./components/tesimonials";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Button variant="tertiary" className="fixed top-4 right-4 z-30">
         <DsgnWhizIcon />
         Join DsgnWhiz
       </Button>
-      <Hero />
-      <div className="-mt-[500px] md:-mt-[550px] lg:-mt-[650px] py-10">
-        <div className="absolute">
-          <div className="relative text-center clipped w-[100vw] h-[100svh]">
-            <Image src={"/bg-arc.webp"} alt="" fill loading="lazy" />
+      <main>
+        <Hero />
+        <div className="-mt-[500px] md:-mt-[550px] lg:-mt-[650px] py-10">
+          <div className="absolute">
+            <div className="relative text-center clipped w-[100vw] h-[100svh]">
+              <Image src={"/bg-arc.webp"} alt="" fill loading="lazy" />
+            </div>
+          </div>
+          <SocialProof />
+          <div className="bg-background">
+            <Benefits />
           </div>
         </div>
-        <SocialProof />
-        <div className="bg-background">
-          <Benefits />
+        <div className="py-20">
+          <Features />
         </div>
+        <div className="py-20">
+          <Testimonials />
+        </div>
+        <div className="py-20">
+          <Pricing />
+        </div>
+        <div className="py-20">
+          <FAQs />
+        </div>
+      </main>
+      <div className="pt-20">
+        <Footer />
       </div>
-      <div className="py-20">
-        <Features />
-      </div>
-      <div className="py-20">
-        <Testimonials />
-      </div>
-      <div className="py-20">
-        <Pricing />
-      </div>
-      <div className="py-20">
-        <FAQs />
-      </div>
-    </main>
+    </>
   );
 }
