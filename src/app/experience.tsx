816d2225235c223cbf516fa10b4535f02eaa1b47
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2024-12-09 15:22:07 +0100

    fix media query

--- src/app/experience.tsx
+++ src/app/experience.tsx
@@ -24,14 +24,19 @@ export function Experience({ children }: { children: React.ReactNode }) {
   }, []);
 
   useEffect(() => {
-    const mm = gsap.matchMedia();
-
-    mm.add("(min-width: 768px)", () => {
-      if (lenisRef.current?.lenis) {
+    const mm = window.matchMedia("(min-width: 768px)");
+    function handleResize(event: MediaQueryListEvent) {
+      if (event.matches && lenisRef.current?.lenis) {
         lenisRef.current.lenis.options.infinite = true;
         lenisRef.current.lenis.options.syncTouch = true;
       }
-    });
+    }
+
+    mm.addEventListener("change", handleResize);
+
+    return () => {
+      mm.removeEventListener("change", handleResize);
+    };
   }, []);
 
   return (
